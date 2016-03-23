b0019e7980eb15564b3430d96e18bc7e721cffac
Michael <michael@iMac-Michael.local>
2016-03-23 15:22:17 +0600

    Created moddel, controller and some views for News

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,37 @@
+class NewsController < ApplicationController
+	def index
+		@news = News.find_each
+	end
+
+	def show
+		@news = News.find(:id)
+	end
+
+	def new
+		@news = News.new
+	end
+
+	def create
+		@news = News.new(news_params)
+		if @news.save
+			redirect_to :index
+		else
+			redirect_to :new
+		end
+	end
+
+	def edit		
+	end
+
+	def update		
+	end
+
+	def destroy		
+	end
+
+	private
+
+		def news_params
+			params.require(:news).permit(:title, :content)			
+		end
+end
