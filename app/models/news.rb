6ed6e4fdd9028e31399fe38a3f4b36295d5c44f9
Michael <mixty01@gmail.com>
2016-03-31 16:24:38 +0600

    Changed update method

--- app/models/news.rb
+++ app/models/news.rb
@@ -10,7 +10,7 @@ class News < ActiveRecord::Base
 		def vk_wall_post
 			vk = vk_init
 			vk_response = vk.wall.post owner_id: ENV['vk_group_id'], from_group: 1, signed: 0, message: ActionView::Base.full_sanitizer.sanitize(self.content)
-			self.update_attribute(:vk_post_id, vk_response.post_id)
+			self.update_column(:vk_post_id, vk_response.post_id)
 		end
 
 		def vk_wall_edit
