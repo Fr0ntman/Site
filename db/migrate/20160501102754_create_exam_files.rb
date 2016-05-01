ba6d277d15af8befde2df552bc768ff7a41b1f50
Michael <mixty01@gmail.com>
2016-05-01 16:54:50 +0600

    Add default title for exams & answers files

--- db/migrate/20160501102754_create_exam_files.rb
+++ db/migrate/20160501102754_create_exam_files.rb
@@ -2,7 +2,7 @@ class CreateExamFiles < ActiveRecord::Migration
   def change
     create_table :exam_files do |t|
     	t.belongs_to :exam, index: true
-    	t.string     :title, null: false
+    	t.string     :title, null: false, default: 'Экзамен'
     	t.integer    :number, null: false
     	t.string     :file, null: false
     	t.integer    :year, null: false
--- db/migrate/20160501102810_create_answer_files.rb
+++ db/migrate/20160501102810_create_answer_files.rb
@@ -2,7 +2,7 @@ class CreateAnswerFiles < ActiveRecord::Migration
   def change
     create_table :answer_files do |t|
     	t.belongs_to :exam_file, index: true
-    	t.string     :title, null: false
+    	t.string     :title, null: false, default: 'Ответы'
     	t.integer    :number, null: false
     	t.string     :file, null: false
 
