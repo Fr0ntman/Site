44df88ea8a2ec0e5676f1a691d971af73b411b68
Michael <mixty01@gmail.com>
2016-05-01 19:47:03 +0600

    Add default title for lectures

--- db/migrate/20160406170356_create_lectures.rb
+++ db/migrate/20160406170356_create_lectures.rb
@@ -2,7 +2,7 @@ class CreateLectures < ActiveRecord::Migration
   def change
     create_table :lectures do |t|
       t.belongs_to :course, index: true
-      t.string     :title, null: false
+      t.string     :title, null: false, default: 'Лекция'
       t.text       :description, null: false
       t.integer    :number
       t.string     :video
