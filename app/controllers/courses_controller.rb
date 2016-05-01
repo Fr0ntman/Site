0f08ddced9eb2ee6bbc104845b196bfef8474e68
Michael <mixty01@gmail.com>
2016-05-01 22:31:06 +0600

    Add index and show methods to courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,12 @@
 class CoursesController < ApplicationController
+	def index
+		@courses = Course.find_each
+	end
+
+	def show
+		course = Course.find params[:id]
+		@exams = course.exams
+		@lectures = course.lectures
+		@tasks = course.tasks
+	end
 end
