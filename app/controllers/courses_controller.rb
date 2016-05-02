eaa0a21861f33719792f3cf7c92199c47de20f36
Michael <mixty01@gmail.com>
2016-05-02 22:34:17 +0600

    Add create method to courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,4 +10,34 @@ class CoursesController < ApplicationController
 	def new
 		@course = Course.new
 	end
+
+	def create
+		@course = Course.new course_params
+		respond_to do |format|
+			if @course.save
+				format.html { redirect_to @course }
+			else
+				format.html { render :new }
+			end
+		end
+	end
+
+	private
+
+		def course_params
+			params.require(:course).permit(
+				:title, 
+				:bg_img,
+				:color, 
+				:description, 
+				:number, 
+				:level, 
+				:original_course_link, 
+				:teacher, 
+				:category, 
+				:sub_category, 
+				:speciality, 
+				:date_of_creating
+			)
+		end
 end
