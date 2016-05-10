626373c63fbb47331afd31a054be4289d9305b5e
Michael <mixty01@gmail.com>
2016-05-10 20:10:42 +0600

    Added VK API errors handler

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,6 @@
 class NewsController < ApplicationController
   before_action :news_item, only: [:show, :edit, :update, :destroy]
+  rescue_from VkontakteApi::Error, with: :vk_error_handler
 
   def index
     @news = News.order(created_at: :desc).all
@@ -50,11 +51,18 @@ class NewsController < ApplicationController
 
   private
 
-  def news_params
-    params.require(:news).permit(:title, :content, :description, attachments: [])
-  end
+    def vk_error_handler
+      respond_to do |format|
+        format.html { render :new } 
+        flash.now[:error] = "Произошла ошибка при обращении к API вконтакте"
+      end
+    end
 
-  def news_item
-    @news = News.find params[:id]
-  end
+    def news_params
+      params.require(:news).permit(:title, :content, :description, attachments: [])
+    end
+
+    def news_item
+      @news = News.find params[:id]
+    end
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -24,39 +24,39 @@ class News < ActiveRecord::Base
 
   private
 
-  def vk_wall_post
-    vk_response = VK_CLIENT.wall.post owner_id: ENV['vk_owner_id'],
-                                      from_group: 1, signed: 0,
-                                      message: ActionView::Base.full_sanitizer.sanitize(content),
-                                      attachments: get_attachments
-    update_column(:vk_post_id, vk_response.post_id)
-  end
-
-  def vk_wall_edit
-    VK_CLIENT.wall.edit owner_id: ENV['vk_owner_id'],
-                        from_group: 1, signed: 0,
-                        message: ActionView::Base.full_sanitizer.sanitize(content),
-                        post_id: vk_post_id,
-                        attachments: get_attachments
-  end
-
-  def vk_wall_delete
-    VK_CLIENT.wall.delete owner_id: ENV['vk_owner_id'],
-                          post_id: vk_post_id
-  end
-
-  def get_attachments
-    upload_url = VK_CLIENT.photos.get_wall_upload_server(group_id: ENV['vk_group_id']).upload_url
-    files = []
-    self.attachments.each do |file|
-      mime_type = MIME::Types.type_for(file.current_path).first.content_type
-      vk_response = VK.upload url: upload_url, file: [file.current_path, mime_type]
-      vk_response.group_id = ENV['vk_group_id']
-      result = VK_CLIENT.photos.save_wall_photo vk_response
-      result.each do |item|
-        files << item.id
-      end
+    def vk_wall_post
+      vk_response = VK_CLIENT.wall.post owner_id: ENV['vk_owner_id'],
+                                        from_group: 1, signed: 0,
+                                        message: ActionView::Base.full_sanitizer.sanitize(content),
+                                        attachments: get_attachments
+      update_column(:vk_post_id, vk_response.post_id)
     end
-    files
-  end
+
+    def vk_wall_edit
+      VK_CLIENT.wall.edit owner_id: ENV['vk_owner_id'],
+                          from_group: 1, signed: 0,
+                          message: ActionView::Base.full_sanitizer.sanitize(content),
+                          post_id: vk_post_id,
+                          attachments: get_attachments
+    end
+
+    def vk_wall_delete
+      VK_CLIENT.wall.delete owner_id: ENV['vk_owner_id'],
+                            post_id: vk_post_id
+    end
+
+    def get_attachments
+      upload_url = VK_CLIENT.photos.get_wall_upload_server(group_id: ENV['vk_group_id']).upload_url
+      files = []
+      self.attachments.each do |file|
+        mime_type = MIME::Types.type_for(file.current_path).first.content_type
+        vk_response = VK.upload url: upload_url, file: [file.current_path, mime_type]
+        vk_response.group_id = ENV['vk_group_id']
+        result = VK_CLIENT.photos.save_wall_photo vk_response
+        result.each do |item|
+          files << item.id
+        end
+      end
+      files
+    end
 end
