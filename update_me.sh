6dea487af6c3b59e881f28b082712249a8cca27e
A. Gareev <me@octogeek.ru>
2016-05-19 18:28:24 +0400

    fix to bot

--- update_me.sh
+++ update_me.sh
@@ -7,15 +7,13 @@ to="#general"
 subject="Deploy"
 emoji=":tophat:"
 
+rvm use 2.2@main
 MessageText=$(git pull origin develop 2>&1)
-bundle install
-RAILS_ENV=production rake assets:clobber
-RAILS_ENV=production rake assets:precompile
+bundle install 1>/dev/null
+RAILS_ENV=production rake assets:clobber 1>/dev/null
+RAILS_ENV=production rake assets:precompile 1>/dev/null
 passenger stop -p 4000
 passenger start -p 4000 -e production -d
 exitstatus=$?
 
-
-message="${subject}: $MessageText"
-payload="payload={\"channel\": \"${to}\", \"username\": \"${username}\", \"text\": \"${message}\", \"icon_emoji\": \"${emoji}\"}"
-curl -m 5 --data-urlencode "${payload}" $url
+echo $MessageText
