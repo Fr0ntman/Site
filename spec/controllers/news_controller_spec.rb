8f9a3a8952cfb77dbc955f9971a3a8faf3f0fb4a
bger <boris.gerlovsky@gmail.com>
2016-06-17 16:10:30 +0300

    Added tests for PagesController. Added needed gems.

--- spec/controllers/news_controller_spec.rb
+++ spec/controllers/news_controller_spec.rb
@@ -1,23 +1,39 @@
 require "rails_helper"
 
 RSpec.describe NewsController, :type => :controller do
+  let(:news) { create_list(:new_post, 5) }
+  
   describe "GET #index" do
-    it "responds successfully with an HTTP 200 status code" do
+    before(:each) do
       get :index
+    end
+    
+    it "responds successfully with an HTTP 200 status code" do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "renders the index template" do
-      get :index
       expect(response).to render_template("index")
     end
 
     it "loads all of the news into @news" do
-      news1, news2 = News.create!(title: 'test1', content: 'test1'), News.create!(title: 'test2', content: 'test2')
-      get :index
-
-      expect(assigns(:news)).to match_array([news1, news2])
+      expect(assigns(:news)).to match_array(news)
+    end
+  end
+  
+  describe "GET #show" do
+    before(:each) do
+      get :show, id: news.first.id
+    end
+    
+    it "takes four last news" do
+      last_news_count = 4
+      expect(assigns(:last_news)).to match_array(news.last(last_news_count))
+    end
+    
+    it "takes requested news" do
+      expect(assigns(:news)).to eq(news.first)
     end
   end
 end
