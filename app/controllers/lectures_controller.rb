ca41acad8aafd870f35cb01f79abfa5a984924fe
Michael <mixty01@gmail.com>
2016-07-20 01:39:17 +0600

    Fixed link on lecture page

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -4,6 +4,6 @@ class LecturesController < ApplicationController
 	def show
 		@course = Course.find params[:course_id]
 		@lecture = @course.lectures.find params[:id]
-		@speciality = Speciality.find @course.id
+		@speciality = Speciality.find @course.speciality
 	end
 end
