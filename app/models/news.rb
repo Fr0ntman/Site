feafcb8e4a70240abd59cbb62aacced9744ab657
Michael <mixty01@gmail.com>
2016-08-05 02:46:51 +0600

    Fixed sort component on speciality page

--- app/models/news.rb
+++ app/models/news.rb
@@ -17,10 +17,10 @@ class News < ActiveRecord::Base
 
   belongs_to :category, class_name: 'NewsCategory'
 
-  validates :title, presence: true, length: { maximum: 100 }
+  validates :title, presence: true #, length: { maximum: 100 }
   validates :content, presence: true
-  validates :category_id, presence: true
-  validates :description, length: { maximum: 220 }
+  #validates :category_id, presence: true
+  #validates :description, length: { maximum: 220 }
 
   scope :published, -> { where(published: true) }
   scope :unpublished, -> { where(published: false) }
